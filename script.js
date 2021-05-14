5ef7834db56e3473031250fd246273fa121ca4b2
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-05-14 08:53:51 +0200

    github

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 const boxes = document.querySelectorAll('.box');
 
+const links = [
+  'https://silvesterspath.github.io/github_finder/',
+  'https://silvesterspath.github.io/Incrementing-Counter/',
+  'https://silvesterspath.github.io/Animated-Navigation/',
+  'citrom',
+  'szilva',
+  'repa',
+  'retek',
+  'mogyoro',
+  'koran',
+  'reggel',
+  'ritkán',
+  'rikkant',
+  'a',
+  'rigó',
+];
+
+const images = [
+  './img/github_finder.png',
+  './img/counter.png',
+  './img/animated_nav.png',
+];
+
+const texts = [
+  'Gihub Finder',
+  'Incrementing Counter',
+  'Animated Navigation',
+  'w5',
+];
+
 window.addEventListener('scroll', checkBoxes);
 
 checkBoxes();
@@ -7,7 +37,12 @@ checkBoxes();
 function checkBoxes() {
   const trigger = (window.innerHeight / 5) * 3;
 
-  boxes.forEach((i) => {
+  boxes.forEach((i, index) => {
+    i.style.backgroundImage = `url(${images[index]})`;
+    i.style.backgroundSize = 'cover';
+
+    i.setAttribute('href', `${links[index]}`);
+    i.innerHTML = `<h3 style="opacity: 1">${texts[index]}</h3>`;
     const boxTop = i.getBoundingClientRect().top;
 
     if (boxTop < trigger) {
